d4bf36960d66abe1cd34ffe70991fdc49bb72491
yusuf233 <sufhidayat9@gmail.com>
2024-02-26 11:51:43 +0700

    add variable Jejari and Luas

---
+++ P1/P1.cpp
@@ -0,0 +1,15 @@
+// P1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double nJejari, nLuas;
+	
+}
+
+
+
+
